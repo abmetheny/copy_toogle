7bfd18ca83e09c1562272fb57afcd5c972b8e727
abmetheny <abmetheny@gmail.com>
2023-06-07 19:27:05 -0400

    new files

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,13 +1,4 @@
 module.exports = {
-content: [
-    './src/pages/**/*.{html,js}',
-    './src/components/**/*.{html,js}',
-    './src/components/**/**/*.{html,js}',
-    './public/index.html',
-    './node_modules/daisyui/src/index.js',
-  ],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   plugins: [require('daisyui')],
-  daisyui: {
-    themes: ["light"]
-  },
 };
