a09330642d5414ce6ca48d0571fcdf12362092c7
abmetheny <abmetheny@gmail.com>
2023-06-07 20:38:47 -0400

    added daisyui folder to client folder

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,4 @@
-const plugin = require('tailwindcss/plugin')
-
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
-  plugins: [
-    plugin(require('daisyui'))
-  ],
+  plugins: [require('daisyui')],
 };
