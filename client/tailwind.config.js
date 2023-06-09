87493252dc7476f2225a827c94df1a9828dedf6e
abmetheny <abmetheny@gmail.com>
2023-06-09 14:09:01 -0400

    changed to index.js extension

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,4 +1,4 @@
-import daisy from './node_modules/daisyui/src/index.d.ts';
+import daisy from './node_modules/daisyui/src/index.js';
 
 module.exports = {
     content: ["./src/**/*.{js,jsx,ts,tsx}", 'node_modules/daisyui/dist/**/*.js', 'node_modules/react-daisyui/dist/**/*.js'],
