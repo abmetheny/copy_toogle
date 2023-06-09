ae80de118886c8f5db00b4f32b495be906d38164
abmetheny <abmetheny@gmail.com>
2023-06-09 14:01:12 -0400

    added file extension

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,4 +1,4 @@
-import daisy from './node_modules/daisyui/src/index';
+import daisy from './node_modules/daisyui/src/index.d.ts';
 
 module.exports = {
     content: ["./src/**/*.{js,jsx,ts,tsx}", 'node_modules/daisyui/dist/**/*.js', 'node_modules/react-daisyui/dist/**/*.js'],
